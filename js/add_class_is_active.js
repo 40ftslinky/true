d66b65a6910627c7cf6bb23ef29cc0e0994cedca
Jeremy Nicholson <40289122+40ftslinky@users.noreply.github.com>
2021-07-20 15:16:52 +1000

    fix JS on mobile nav

--- js/add_class_is_active.js
+++ js/add_class_is_active.js
@@ -16,13 +16,31 @@ jQuery(document).ready(function( $ ) {
             $(".menu-icons").remove("is-active");
         }
     }); 
-    
+
     $(".menu_list .menu-link").on("click", function() {
         $(this).find(".menu_dropdown").slideToggle();
         // $(".menu-link").find(".is-active").remove("is-active");
         $(this).toggleClass("is-active");
-        // $(".menu-icons").find(".is-active").remove("is-active");
-        $(".menu-icons").toggleClass("is-active");
+        // Ross   changes  27/7/21
+        // set counter
+		var count_active = 0;			
+		// loops through all a links in menu
+		var list_links = jQuery("#nav .menu_list .menu_item .menu-link");
+		list_links.each(function(id, a_link) {
+			 // if this menus open? is- active
+			if (jQuery(a_link).hasClass("is-active")){
+                // add to counter
+                count_active++;	 
+			}
+		});	   
+	   // if all are closed remove is-active class
+	   if(count_active==0){
+		   jQuery(".menu-icons").removeClass("is-active");
+	   } else {
+		// else add is-active class
+		 jQuery(".menu-icons").addClass("is-active");
+	   }
+	   // end Ross changes 
     });
     
     $(".pre-scrim").click(function(e){
