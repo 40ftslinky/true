d8f08ed34dd06ab2792baa085eaac3d6b4f27d9f
Jeremy Nicholson <40289122+40ftslinky@users.noreply.github.com>
2021-08-12 12:12:57 +1000

    js hide subtitle

--- js/add_class_swap_state.js
+++ js/add_class_swap_state.js
@@ -4,7 +4,11 @@ jQuery(document).ready(function( $ ) {
 
     $(".case_study_brand_wrap.state_off").fadeOut();
     $(".case_study_brand_wrap.state_off").css("visibility", "visible")
-
+    if ($(".case_study_brand_wrap.state_on")[0]){
+        // Do something if class exists
+    } else {
+        // Do something if class does not exist
+    }
 
     $(".swap_state_on").click(function(){
         $("div.state_on").fadeOut();
@@ -13,6 +17,8 @@ jQuery(document).ready(function( $ ) {
         // change btn state
         $(this).removeClass('btn-outline-white green-OL-txt').addClass('btn-white blue-txt');
         $(".swap_state_off").removeClass('btn-white green-txt').addClass('btn-outline-white blue-OL-txt');
+		$("#subtitle1").hide();
+		$("#subtitle2").show();
     });
     $(".swap_state_off").click(function(){
         $("div.state_on").fadeIn();
@@ -21,8 +27,12 @@ jQuery(document).ready(function( $ ) {
         // change btn state
         $(this).removeClass('btn-outline-white blue-OL-txt').addClass('btn-white green-txt');
         $(".swap_state_on").removeClass('btn-white blue-txt').addClass('btn-outline-white green-OL-txt');
-        
+		$("#subtitle1").show();
+		$("#subtitle2").hide();
+
     });
 
 });
+
+
 
